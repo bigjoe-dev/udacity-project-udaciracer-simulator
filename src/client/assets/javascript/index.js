1340c7a3e03560bafe129f8dd95d933201f0ec10
Joe Delamore <jmgdelamore@gmail.com>
2021-12-13 10:40:17 +0100

    got player_id and track_id from store and created race via API then stored id in store

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -75,7 +75,7 @@ async function delay(ms) {
 // This async function controls the flow of the race, add the logic and error handling
 async function handleCreateRace() {
 	// render starting UI
-	//renderAt('#race', renderRaceStartView())
+	//enderAt('#race', renderRaceStartView({name: 'test'}))
 
 	// TODO - Get player_id and track_id from the store
 	const player_id = store.player_id
@@ -84,9 +84,10 @@ async function handleCreateRace() {
 	// const race = TODO - invoke the API call to create the race, then save the result
 	const race = createRace(player_id, track_id)
 
-	console.log(race)
-
 	// TODO - update the store with the race id
+	race.then((race) => {
+		store.race_id = race.ID
+	})
 
 	// The race has been created, now start the countdown
 	// TODO - call the async function runCountdown
